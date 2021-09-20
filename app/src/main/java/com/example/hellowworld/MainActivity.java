a060c220ae3b4be616ae8e9b505fe0d231e83d2c
kevin fang <kfang22@wisc.edu>
2021-09-19 19:37:29 -0500

    Initial commit

---
+++ app/src/main/java/com/example/hellowworld/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.hellowworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hellow World");
+    }
+}
